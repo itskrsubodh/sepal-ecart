9fc6846c20a0309077e452fb9e69d2d35f04044f
subodh kumar <itskrsubodh@gmail.com>
2024-03-21 09:18:44 +0530

    user detail component added

--- sepal.ClientApp/src/app/app.module.ts
+++ sepal.ClientApp/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserdetailComponent } from './feature/userdetail/userdetail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserdetailComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
